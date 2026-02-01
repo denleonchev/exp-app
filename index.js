a19c8caeb1fa47c0c05ac773aac4dbb64f324b67
den.leonchev@gmal.com <den.leonchev@gmail.com>
2026-02-01 15:53:07 +0000

    add middlewares

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ import { usersRouter } from './users/users.js';
 const port = 8000;
 const app = express();
 
+app.use((req, res, next) => {
+  console.log(`Time is `, new Date().toLocaleString());  
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Hi');
 });
 
 app.use('/users', usersRouter);
+
+usersRouter.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send(err.message)
+  next();
+});
 
 app.listen(port, () => {
   console.log(`Server is running on localhost:${port}`); 
--- users/users.js
+++ users/users.js
@@ -2,7 +2,13 @@ import express from 'express';
 
 export const usersRouter = express.Router();
 
+usersRouter.use((req, res, next) => {
+  console.log('Users middleware');  
+  next();
+});
+
 usersRouter.post('/login', (req, res) => {
+  throw new Error('bad')
   res.send('login');
 });
 
