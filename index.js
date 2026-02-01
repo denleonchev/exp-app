5ba7ec0ba6ab9ef75d4033dc7b7344a6a127033d
den.leonchev@gmal.com <den.leonchev@gmail.com>
2026-02-01 15:31:48 +0000

    add users router

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { usersRouter } from './users/users.js';
 
 const port = 8000;
 const app = express();
@@ -6,6 +7,8 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hi');
 });
+
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on localhost:${port}`); 
