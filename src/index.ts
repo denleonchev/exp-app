5ef3c9ba1023fdef44940fb11627778578c464e4
den.leonchev@gmal.com <den.leonchev@gmail.com>
2026-02-10 23:13:07 +0000

    improve use of ioc

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-import { Container } from 'inversify';
+import {
+  Container,
+  ContainerModule,
+  ContainerModuleLoadOptions,
+} from 'inversify';
 import { App } from './app';
 import { ExceptionFilter } from './errors/exception.filter';
 import { LoggerService } from './logger/logger.service';
@@ -6,15 +10,18 @@ import { UsersController } from './users/users.controller';
 import { ILogger } from './logger/logger.interface';
 import { dependencyType } from './dependencyTypes';
 import { IExceptionFilter } from './errors/exception.filter.interface';
+import { ABaseController } from './common/base.controller';
+
+const containerModule = new ContainerModule(
+  ({ bind }: ContainerModuleLoadOptions) => {
+    bind<App>(dependencyType.app).to(App);
+    bind<ILogger>(dependencyType.iLogger).to(LoggerService);
+    bind<ABaseController>(dependencyType.userConroller).to(UsersController);
+    bind<IExceptionFilter>(dependencyType.exceptionFilter).to(ExceptionFilter);
+  }
+);
 
 const dependencyContainer = new Container();
-dependencyContainer.bind<App>(dependencyType.app).to(App);
-dependencyContainer.bind<ILogger>(dependencyType.iLogger).to(LoggerService);
-dependencyContainer
-  .bind<UsersController>(dependencyType.userConroller)
-  .to(UsersController);
-dependencyContainer
-  .bind<IExceptionFilter>(dependencyType.exceptionFilter)
-  .to(ExceptionFilter);
+dependencyContainer.load(containerModule);
 const app = dependencyContainer.get<App>(dependencyType.app);
 app.main();
