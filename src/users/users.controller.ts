671bd660bbf9c5498ff5f89d9f2cfa777dfcaf1d
den.leonchev@gmal.com <den.leonchev@gmail.com>
2026-02-13 21:02:01 +0000

    Add jwt

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,12 +9,15 @@ import { UserRegisterDTO } from './dto/user-register.dto';
 import { UserLoginDTO } from './dto/user-login.dto';
 import { IUsersService } from './users.service.interface.';
 import { ValidateMiddleware } from '../common/validate.middleware';
+import { sign } from 'jsonwebtoken';
+import { IConfigService } from '../config/config.service.interface';
 
 @injectable()
 export class UsersController extends ABaseController {
   constructor(
     @inject(dependencyType.iLogger) logger: ILogger,
-    @inject(dependencyType.usersService) private usersService: IUsersService
+    @inject(dependencyType.usersService) private usersService: IUsersService,
+    @inject(dependencyType.configService) private configService: IConfigService
   ) {
     super(logger);
     this.bindRoutes([
@@ -53,6 +56,34 @@ export class UsersController extends ABaseController {
     if (!validationResult) {
       return next(new HTTPError(401, 'Auth error', 'login'));
     }
-    this.ok(res, 'login');
+    const secret = this.configService.get('SECRET');
+    if (!secret) {
+      throw new Error('SECRET env var is required');
+    }
+    const jwt = await this.signJWT(req.body.email, secret);
+    this.ok(res, { jwt });
+  }
+
+  private async signJWT(email: string, secret: string) {
+    return await new Promise<string>((resolve, reject) => {
+      sign(
+        { email, iat: Math.floor(Date.now() / 1000) },
+        secret,
+        { algorithm: 'HS256' },
+        (err, token) => {
+          if (err) {
+            return reject(err);
+          }
+
+          if (!token) {
+            return reject(
+              new Error('There are problems with token generation')
+            );
+          }
+
+          resolve(token);
+        }
+      );
+    });
   }
 }
